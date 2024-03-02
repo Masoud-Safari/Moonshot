ae102e8a99e547117f887782d90e0a7beb1fa4ba
Masoud Safari <mmsafari@gmail.com>
2024-03-02 15:46:48 -0800

    Initial commit.

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Masoud Safari on 2024-03-02.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
