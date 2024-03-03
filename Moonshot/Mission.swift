e6d92f8ea80561aa5ebfd70b5b5dc97bcebbe07d
Masoud Safari <mmsafari@gmail.com>
2024-03-02 16:43:52 -0800

    Add a generic function to read JSON files.

---
+++ Moonshot/Mission.swift
@@ -0,0 +1,22 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Masoud Safari on 2024-03-02.
+//
+
+import Foundation
+
+
+struct Mission: Codable, Identifiable {
+    
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
