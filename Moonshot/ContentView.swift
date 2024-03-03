0cebba90e016a1fbd08ca8c2d958869ca78be68e
Masoud Safari <mmsafari@gmail.com>
2024-03-03 14:50:50 -0800

    Add list view and a toggle on the main view to switch between list view and grid view.

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -10,15 +10,24 @@ import SwiftUI
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    @State private var showingGrid  = true
 
     var body: some View {
         NavigationStack {
             Group {
-                GridLayout(astronauts: astronauts, missions: missions)
+                if showingGrid {
+                    GridLayout(astronauts: astronauts, missions: missions)
+                } else {
+                    ListLayout(astronauts: astronauts, missions: missions)
+                }
             }
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            .toolbar {
+                Toggle("Grid View", isOn: $showingGrid)
+            }
         }
     }
 }
--- Moonshot/ListLayout.swift
+++ Moonshot/ListLayout.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct ListLayout: View {
+    let astronauts: [String: Astronaut]
+    let missions: [Mission]
+    
+    //    let columns = [
+    //        GridItem(.adaptive(minimum: 150))
+    //    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ForEach(missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission, astronauts: astronauts)
+                } label: {
+                    HStack {
+                        HStack {
+                            VStack {
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                    .foregroundStyle(.white)
+                                
+                                Text(mission.formattedLaunchDate)
+                                    .font(.caption)
+                                    .foregroundStyle(.gray)
+                            }
+                            .padding(.horizontal, 50)
+                            
+                            Spacer()
+                        }
+                        .padding(.vertical)
+                        .frame(maxWidth: .infinity)
+                        
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                            .padding(.horizontal, 50)
+                    }
+                    .background(.lightBackground)
+                    .clipShape(.rect(cornerRadius: 10))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.lightBackground)
+                    )
+                    .padding(.horizontal, 20)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    ListLayout()
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    return ListLayout(astronauts: astronauts, missions: missions)
+        .preferredColorScheme(.dark)
 }
